0bbd4651591de84f3e518211857113f994fc242d
Sarah Hale <sarahhale@Sarahs-MBP.lan>
2018-11-27 23:23:05 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,16 +1,26 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay.push('cardOne');
-console.log("User flipped queen");
-var cardTwo = cards[2];
-cardsInPlay.push('cardTwo');
-console.log("User flipped king");
-if (cardsInPlay.length === 2) {
+
+var checkForMatch = function () {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 		alert("You found a match!");
 	} else {
 		alert("Sorry, try again.");
 	}
+};
+
+var flipCard = function (cardId) {
+	checkForMatch();
+	var cardOne = cards[0];
+	
+if (cardsInPlay.length === 2) {
+
+	cardsInPlay.push(cards[cardId]);
+
 }
+console.log("User flipped " + cards[cardId]);
+
+};
+flipCard(0);
+flipCard(2);
 
